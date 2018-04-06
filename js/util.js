c49329b2a4e52d65a2725e2f488013cb944617bd
Leo <527261787@qq.com>
2018-04-06 08:44:51 +0800

    4.5 update bug

--- js/util.js
+++ js/util.js
@@ -40,44 +40,42 @@ var util = {
 				errback();
 			}
 		});
-	},
-	/**
-	 *	文件下载 
-	 * @param {Object} filePath 文件路径
-	 */
-	fileDown: function(filePath){
-		console.log(filePath);
-		var url = "http://snip.qq.com/resources/Snip_V2.0_5771.dmg";
-//		var url = downUrl.left + filePath + downUrl.right;
-		console.log(url);
-		var options = {method:"GET"};
-		var dtask = plus.downloader.createDownload( url, options);
-		console.log(dtask.filename);
-		dtask.addEventListener( "statechanged", function(task, status){
-			if(!dtask){ 
-				mui.toast('下载任务创建失败');
-				return; 
-			}
-			else{
-				dtask.flag = true;
-				mui.toast('下载任务创建成功');
-			}
-			switch(task.state) {
-				case 1: // 开始
-					mui.toast( '开始下载...');
-					break;
-				case 2: // 已连接到服务器
-				    console.log( "链接到服务器..." );
-				    break;
-				case 3:	// 已接收到数据
-				    console.log(task.downloadedSize + "/" + task.totalSize );
-				    break;
-				case 4:	// 下载完成
-				    mui.toast(dtask.filename + '下载完成！' );
-				    dtask.flag = 0;
-				    break;
-			}
-		});
-		return dtask;
 	}
 }
+
+// 对Date的扩展，将 Date 转化为指定格式的String  
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，   
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)   
+// 例子：   
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423   
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18   
+  
+Date.prototype.Format = function(fmt){  
+    var o = {  
+         "M+": this.getMonth()+1,  
+         "d+": this.getDate(),  
+         "H+": this.getHours(),  
+         "m+": this.getMinutes(),  
+         "s+": this.getSeconds(),  
+         "S+": this.getMilliseconds()  
+    };  
+  
+    //因位date.getFullYear()出来的结果是number类型的,所以为了让结果变成字符串型，下面有两种方法：  
+  
+      
+  
+    if(/(y+)/.test(fmt)){  
+        //第一种：利用字符串连接符“+”给date.getFullYear()+""，加一个空字符串便可以将number类型转换成字符串。  
+  
+        fmt=fmt.replace(RegExp.$1,(this.getFullYear()+"").substr(4-RegExp.$1.length));  
+    }  
+    for(var k in o){  
+        if (new RegExp("(" + k +")").test(fmt)){  
+  
+            //第二种：使用String()类型进行强制数据类型转换String(date.getFullYear())，这种更容易理解。  
+  
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(String(o[k]).length)));  
+        }  
+    }     
+    return fmt;  
+}  
